6f5bd470c3dd2eee9ecc9a327873e1702b920bd7
Indra Eka Prasetya <indraep92@gmail.com>
2013-04-21 23:15:14 +0700

    ubah style edit text

--- gen/com/example/srt_droid/R.java
+++ gen/com/example/srt_droid/R.java
@@ -25,11 +25,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int lihat_sejarah_penjualan=0x7f020002;
-        public static final int login=0x7f020003;
-        public static final int login1=0x7f020004;
-        public static final int user=0x7f020005;
+        public static final int edittext_custom=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int lihat_sejarah_penjualan=0x7f020003;
+        public static final int login=0x7f020004;
+        public static final int login1=0x7f020005;
+        public static final int textfield_activated_holo_light=0x7f020006;
+        public static final int textfield_default_holo_light=0x7f020007;
+        public static final int textfield_disabled_focused_holo_light=0x7f020008;
+        public static final int textfield_disabled_holo_light=0x7f020009;
+        public static final int textfield_focused_holo_light=0x7f02000a;
+        public static final int user=0x7f02000b;
     }
     public static final class id {
         public static final int action_settings=0x7f090026;
@@ -149,17 +155,19 @@ public final class R {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int EditTextAppTheme=0x7f070002;
     }
 }
